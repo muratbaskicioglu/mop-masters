3735ebc9f09011fd4a88406fc9e99d566d352b30
Murat Baskıcıoğlu <murat.baskicioglu@getir.com>
2020-09-06 10:54:23 +0300

    feat(api-doc): add open api documentation with NelmioApiDocBundle configuration

--- src/Controller/CleanerController.php
+++ src/Controller/CleanerController.php
@@ -3,17 +3,35 @@
 namespace App\Controller;
 
 use App\Repository\CleanerRepository;
+use App\Entity\Cleaner;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Serializer\SerializerInterface;
+use Nelmio\ApiDocBundle\Annotation\Model;
+use Nelmio\ApiDocBundle\Annotation\Security;
+use Swagger\Annotations as SWG;
 
-
+/**
+ * @Route("/cleaners", name="cleaner_")
+ */
 class CleanerController extends AbstractController
 {
     /**
-     * @Route("/cleaners", name="app_cleaner_list")
+     * @Route(name="list", methods={"GET"})
+     * @SWG\Response(
+     *     response=200,
+     *     description="Returns the rewards of an user",
+     *     @SWG\Schema(
+     *        type="array",
+     *        @SWG\Items(ref=@Model(type=Cleaner::class, groups={"cleaner_list"}))
+     *     )
+     * )
+     * @SWG\Tag(name="cleaners")
+     * @Security(name="Bearer")
      *
      * @param CleanerRepository $cleanerRepository
      * @param SerializerInterface $serializer
